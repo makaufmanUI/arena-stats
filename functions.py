49679b533c7d17a596299606f2793963a6dd36aa
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2022-11-24 11:19:53 -0600

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+
+
+def plot_rating(df: pd.DataFrame, showEnemy: bool = False):
+    fig, ax = plt.subplots()
+    fig.tight_layout()
+    ratings = df['newTeamRating'].astype(int)
+    ax.plot(ratings, color='#83c9ff')
+    ax.set_xlabel('Match', fontsize=10, color='#ebebd6', labelpad=15)
+    ax.tick_params(axis='x', which='major', labelsize=10, color='#0e1117', labelcolor='#ebebd6', pad=10)
+    ax.tick_params(axis='y', which='major', labelsize=10, color='#0e1117', labelcolor='#ebebd6', pad=10)
+    if showEnemy:
+        ratings = df['enemyNewTeamRating'].astype(int)
+        ax.plot(ratings, color='xkcd:light red')
+    ax.set_facecolor('#0e1117')
+    fig.set_facecolor('#0e1117')
+    return fig
+
+
+
+
+
+def sort_tuple(tup: tuple) -> tuple:
+    """
+    Sorts the given tuple so that the elements are in alphabetical order.
+    """
+    tup = sorted(tup, key=str.lower)
+    return tup
