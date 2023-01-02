635259265b41fd380151acb78364d13a850637fe
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-01-02 00:27:06 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,7 +10,7 @@ def plot_rating(df: pd.DataFrame, showEnemy: bool = False):
     ratings = df['newTeamRating'].astype(int)
     max_rating = max(ratings)
     ax.plot(ratings, color='#83c9ff')
-    ax.hline(max_rating)
+    plt.axhline(max_rating)
     ax.set_xlabel('Match', fontsize=10, color='#ebebd6', labelpad=15)
     ax.tick_params(axis='x', which='major', labelsize=10, color='#0e1117', labelcolor='#ebebd6', pad=10)
     ax.tick_params(axis='y', which='major', labelsize=10, color='#0e1117', labelcolor='#ebebd6', pad=10)
