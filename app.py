6bb0ce5262010b2e8f4ec71097020aeb9a42857d
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-01-02 00:15:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ with st.form(key='import_form'):
 
 if st.session_state.arena_data is not None:
     big_spacer()
-    st.write(st.session_state.arena_data)
+    st.write(st.session_state.arena_data[::-1])
 #     st.write(st.session_state.arena_data.reverse())
     small_spacer()
     st.markdown("---")
