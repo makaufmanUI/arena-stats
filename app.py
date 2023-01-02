77b90ccf680b1a7f99e6444311e1e1cb67fb3083
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-01-02 00:47:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ if st.session_state.arena_data is not None:
             winrate_dataframe['comp'] = f"{', '.join(classes)}"
         winrate_dataframe = winrate_dataframe.append({'comp': f"{', '.join(classes)}", 'winrate': winrates[comp]*100, 'games': games}, ignore_index=True)
     
-    #st.write(winrate_dataframe)
+    st.write(winrate_dataframe)
     
     
     for comp in winrates:
