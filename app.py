19f3594ce2216612a86d42b63754e0b685bd647c
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2023-01-02 00:29:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -70,13 +70,13 @@ if st.session_state.arena_data is not None:
     small_spacer()
     st.markdown("### 2v2")
     small_spacer()
-    st.pyplot(plot_rating(get_2v2_matches(st.session_state.arena_data), showEnemy=True))
+    st.pyplot(plot_rating(get_2v2_matches(st.session_state.arena_data), showEnemy=False))
     small_spacer()
     big_spacer()
     small_spacer()
     st.markdown("### 3v3")
     small_spacer()
-    st.pyplot(plot_rating(get_3v3_matches(st.session_state.arena_data), showEnemy=True))
+    st.pyplot(plot_rating(get_3v3_matches(st.session_state.arena_data), showEnemy=False))
 
     
     small_spacer()
